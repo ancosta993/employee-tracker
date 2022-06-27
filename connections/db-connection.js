c517bd104446f198c117f7c58e8a5ec644794d9b
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-26 20:47:01 -0400

    added dotenv for pass protection

--- connections/db-connection.js
+++ connections/db-connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // Connect to database
 const db = mysql.createConnection(
    {
       host: 'localhost',
-      user: 'root',
-      password: 'Sjeet2bang2000oct//QL',
-      database: 'company'
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
    },
    console.log("Connected to the company database")
 );
