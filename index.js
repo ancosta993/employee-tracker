44c486d0302013af903add9a3631bbccf5750f00
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-26 20:24:26 -0400

    added imporved loop where necessary

--- index.js
+++ index.js
@@ -17,78 +17,89 @@ const deleteRole = require('./promise-handler/delete-role');
 
 const initialPrompts = require('./utils/initial-prompts')
 
-initialPrompts().then(ansObj => {
+const main = () => {
+   initialPrompts().then(ansObj => {
 
-   if (ansObj.answer === "View All Departments") {
-      viewDataInst.getAllDept().then(result => {
-         console.table(result);
-      }).catch(err => {
-         console.log("Promise rejected: " + err);
-      });
+      if (ansObj.answer === "View All Departments") {
+         viewDataInst.getAllDept().then(result => {
+            console.table(result);
+            main();
+         }).catch(err => {
+            console.log("Promise rejected: " + err);
+         });
+   
+      } else if (ansObj.answer === "View All Roles"){
+         viewDataInst.getAllRoles().then(result => {
+            console.table(result);
+            main();
+         }).catch(err => {
+            console.log("Promise rejected: "+ err)
+         });
+   
+   
+      } else if (ansObj.answer === "View All employees") {
+         viewDataInst.getAllEmps().then(result => {
+            console.table(result);
+            main();
+         }).catch(err => {
+            console.log("Promise rejected: "+err);
+         });
+   
+   
+      } else if (ansObj.answer == "Add a Department") {
+         addDataInst.addDept(ansObj.newDept);
+         viewDataInst.getAllDept().then(result => {
+            main();
+         }).catch(err => {
+            console.log("Promise rejected: " + err);
+         });
+   
+      } else if(ansObj.answer == "Add a Role") {
+         addDataInst.addRole([ansObj.newRole, ansObj.newRoleSalary, ansObj.newRoleDept]);
+         viewDataInst.getAllRoles().then(result => {
+            main();
+         }).catch(err => {
+            console.log("Promise rejected: "+ err)
+         });
+   
+   
+      } else if(ansObj.answer == "Add an employee") {
+         console.log(ansObj);
+         addDataInst.addEmp([ansObj.empFirstName, ansObj.empLastName, ansObj.empRole, ansObj.empManager]);
+         viewDataInst.getAllEmps().then(result=> {
+            main();
+         });
+   
+   
+      } else if (ansObj.answer === "Update an Employee Role") {
+   
+         updateEmpRole();
+         
+   
+      } else if (ansObj.answer === "Update employee managers"){
+         updateEmpManager();
+         
+      } else if(ansObj.answer === 'Get employees by managers'){
+         viewDataInst.getEmpsByMans().then(result => {
+            console.table(result);
+            main();
+         });
+      } else if(ansObj.answer === 'Get employees by departments'){
+         viewDataInst.getEmpsByDept().then(result => {
+            console.table(result);
+            main();
+         });
+      } else if(ansObj.answer === "Delete A Department"){
+         deleteDept();
+   
+      } else if (ansObj.answer === "Delete A Role") {
+         deleteRole()
+      } else {
+         console.log('No Match');
+      }
+   });
+}
 
-   } else if (ansObj.answer === "View All Roles"){
-      viewDataInst.getAllRoles().then(result => {
-         console.table(result);
-      }).catch(err => {
-         console.log("Promise rejected: "+ err)
-      });
-
-
-   } else if (ansObj.answer === "View All employees") {
-      viewDataInst.getAllEmps().then(result => {
-         console.table(result);
-      }).catch(err => {
-         console.log("Promise rejected: "+err);
-      });
-
-
-   } else if (ansObj.answer == "Add a Department") {
-      addDataInst.addDept(ansObj.newDept);
-      viewDataInst.getAllDept().then(result => {
-         console.table(result);
-      }).catch(err => {
-         console.log("Promise rejected: " + err);
-      });
-
-   } else if(ansObj.answer == "Add a Role") {
-      addDataInst.addRole([ansObj.newRole, ansObj.newRoleSalary, ansObj.newRoleDept]);
-      viewDataInst.getAllRoles().then(result => {
-         console.table(result);
-      }).catch(err => {
-         console.log("Promise rejected: "+ err)
-      });
-
-
-   } else if(ansObj.answer == "Add an employee") {
-      console.log(ansObj);
-      addDataInst.addEmp([ansObj.empFirstName, ansObj.empLastName, ansObj.empRole, ansObj.empManager]);
-      viewDataInst.getAllEmps().then(result=> console.table(result)).catch(err=> console.log("Promise Rejected: " + err));
-
-
-   } else if (ansObj.answer === "Update an Employee Role") {
-
-      updateEmpRole();
-
-   } else if (ansObj.answer === "Update employee managers"){
-      updateEmpManager();
-      
-   } else if(ansObj.answer === 'Get employees by managers'){
-      viewDataInst.getEmpsByMans().then(result => {
-         console.table(result);
-      });
-   } else if(ansObj.answer === 'Get employees by departments'){
-      viewDataInst.getEmpsByDept().then(result => {
-         console.table(result);
-      });
-   } else if(ansObj.answer === "Delete A Department"){
-      deleteDept();
-
-   } else if (ansObj.answer === "Delete A Role") {
-      deleteRole()
-   } else {
-      console.log('No Match');
-   }
-});
-
+main();
 
 
